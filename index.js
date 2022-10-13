dc693e38dc611f4fa8dc0ef8882148cd1729da2e
Darius <martindarius01@gmail.com>
2022-10-12 22:40:08 -0400

    yoo

---
+++ index.js
@@ -0,0 +1,37 @@
+var a = 1;
+var a = 2;
+var a = 3;
+var a = 4;
+
+console.log(a + b);
+console.log(c + d);
+
+var a = 1;
+var a = 2;
+var a = 3;
+var a = 4;
+
+console.log(a - b);
+console.log(c - d);
+
+var a = 1;
+var a = 2;
+var a = 3;
+var a = 4;
+
+console.log(a * b);
+console.log(c * d);
+
+var a = 1;
+var a = 2;
+var a = 3;
+var a = 4;
+
+console.log(a / b);
+console.log(c / d);
+
+var a ++
+var b ++
+
+var c --
+var d --
